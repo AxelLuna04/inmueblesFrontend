3c5efc3bedbcf938e13d704428b9201d1eaf6862
SaulBT <barragansaul26n@gmail.com>
2025-12-30 16:54:37 -0600

    feat: mostrar historial

---
+++ src/api/historialService.js
@@ -0,0 +1,96 @@
+import { ErrorApi } from '../errors/errorApi.js';
+
+const API_BASE = import.meta.env.VITE_API_BASE || "/api";
+const HISTORIAL_URL = `${API_BASE}/v1/publicaciones`
+
+export async function getHistorial(id) {
+    const res = await fetch(`${HISTORIAL_URL}/${id}/historial`, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
+        }
+    });
+
+    if (!res.ok) {
+        const resJson = await res.json();
+        const message = stringOrNull(resJson.error);
+        if (res.status === 500) {
+            console.error(`Error del servidor - (${res.status}): ${message}`)
+            throw new ErrorApi("Error interno del servidor. Inténtelo de nuevo más tarde.")
+        } else {
+            throw new ErrorApi(message);
+        }
+    }
+
+    return await res.json();
+}
+
+export function getHistorialTest() {
+    const historial = [
+        {
+            idMovimiento: 5,
+            tipoMovimiento: "VENTA",
+            fechaInicio: "2026-05-03",
+            fechaFin: null,
+            nombreCliente: "Enrique Peña Nieto",
+            precio: 80000000,
+            descripcion: "Inmueble vendido"
+        },
+        {
+            idMovimiento: 5,
+            tipoMovimiento: "RENTA",
+            fechaInicio: "2026-04-21",
+            fechaFin: null,
+            nombreCliente: "Erika Monfil Medina",
+            precio: 2500,
+            descripcion: "Inmueble rentado"
+        },
+        {
+            idMovimiento: 5,
+            tipoMovimiento: "RENTA",
+            fechaInicio: "2026-02-13",
+            fechaFin: "2026-04-15",
+            nombreCliente: "Alberto Jaramillo López",
+            precio: 2000,
+            descripcion: "Inmueble rentado"
+        },
+        {
+            idMovimiento: 4,
+            tipoMovimiento: "APROBACION",
+            fechaInicio: "2026-02-09",
+            fechaFin: null,
+            nombreCliente: null,
+            precio: null,
+            descripcion: "Inmueble aprobado"
+        },
+        {
+            idMovimiento: 3,
+            tipoMovimiento: "EDICION",
+            fechaInicio: "2026-02-08",
+            fechaFin: null,
+            nombreCliente: null,
+            precio: null,
+            descripcion: "Inmueble editado"
+        },
+        {
+            idMovimiento: 2,
+            tipoMovimiento: "APROBACION",
+            fechaInicio: "2026-01-27",
+            fechaFin: null,
+            nombreCliente: null,
+            precio: null,
+            descripcion: "Inmueble aprobado"
+        },
+        {
+            idMovimiento: 1,
+            tipoMovimiento: "CREACION",
+            fechaInicio: "2026-01-25",
+            fechaFin: null,
+            nombreCliente: null,
+            precio: null,
+            descripcion: "Inmueble creado"
+        }
+    ]
+
+    return historial;
+}
---
+++ src/js/admin/historial.js
@@ -0,0 +1,155 @@
+import { getListingData } from "../../api/listingsService";
+import { getHistorial, getHistorialTest } from "../../api/historialService";
+import { intOrNull, stringOrNull } from "../../utils/helpers";
+import {
+    showNotif,
+    NOTIF_GREEN,
+    NOTIF_RED,
+    NOTIF_ORANGE
+} from "../../utils/notifications";
+
+//HELPERS
+const $ = (id) => document.getElementById(id);
+
+//STATE
+const state = {
+    id: 0,
+    title: "",
+    movements: []
+}
+
+//ELEMENTS
+const listingTitleLabel = $('listingTitleLabel');
+const movementsContainerDiv = $('movementsContainerDiv');
+
+//INNIT
+document.addEventListener("DOMContentLoaded", innit);
+
+async function innit() {
+    console.log("Inicializando página")
+
+    await loadListingData();
+    listingTitleLabel.innerHTML = state.title;
+    await loadHistorial();
+    displayHistorial();
+
+    console.log("===PÁGINA INICIALIZADA EXITOSAMENTE===")
+}
+
+//LOAD DATA
+async function loadListingData() {
+    console.log("Cargando datos del inmueble");
+
+    const params = new URLSearchParams(window.location.search);
+    state.id = params.get("id");
+
+    if (intOrNull(state.id)) {
+        try {
+            const data = await getListingData(state.id);
+            state.title = data.titulo || "Títutlo de la publicación";
+        } catch(err) {
+            if (err.name === "ErrorApi") return showNotif(err.message, NOTIF_RED, 5000);
+            console.error(`Error del front: ${err}`);
+            showNotif(notification, "No se pudo cargar la publicación, inténtelo de nuevo más tarde.", NOTIF_RED, 5000);
+        }
+    } else {
+        showNotif(notification, "La id del inmueble no es válida.", NOTIF_RED, 5000);
+    }
+
+    console.log("Datos del inmueble cargados")
+}
+
+async function loadHistorial() {
+    try {
+        state.movements = await getHistorial(state.id);
+    } catch(err) {
+        if (err.name === "ErrorApi") return showNotif(err.message, NOTIF_RED, 5000);
+        console.error(`Error del front: ${err}`);
+        showNotif(notification, "No se pudo cargar el historial, inténtelo de nuevo más tarde.", NOTIF_RED, 5000);
+    }
+}
+
+//HISTORIAL
+function displayHistorial() {
+    console.log("Desplegando historial");
+
+    const movementList = [];
+    state.movements.forEach((m) => {
+        const movement = document.createElement("div");
+        movement.classList.add("movement-card", "movement-card-grid");
+        const movementText = document.createElement("div");
+        movementText.classList.add("movement-text");
+        const titleLabel = document.createElement("label");
+        titleLabel.classList.add("movement-title");
+        titleLabel.innerHTML = `<strong>${m.descripcion}</strong>`
+        const dateLabel = document.createElement("label");
+        let innitDate = m.fechaInicio.split('-').reverse().join('/');
+        dateLabel.innerHTML = innitDate;
+        const movementType = document.createElement("div");
+        movementType.innerHTML = '<br>';
+        const clientNameLabel = document.createElement("label");
+        const priceLabel = document.createElement("label");
+
+        switch(m.tipoMovimiento) {
+            case 'CREACION' :
+                movementType.classList.add("movement-type", "movement-creation");
+                movementText.appendChild(titleLabel);
+                movementText.appendChild(dateLabel);
+                movement.appendChild(movementText);
+                movement.appendChild(movementType);
+                break;
+            case 'APROBACION':
+                movementType.classList.add("movement-type", "movement-aprobation");
+                movementText.appendChild(titleLabel);
+                movementText.appendChild(dateLabel);
+                movement.appendChild(movementText);
+                movement.appendChild(movementType);
+                break;
+            case 'EDICION':
+                movementType.classList.add("movement-type", "movement-edition");
+                movementText.appendChild(titleLabel);
+                movementText.appendChild(dateLabel);
+                movement.appendChild(movementText);
+                movement.appendChild(movementType);
+                break;
+            case 'RENTA':
+                if (stringOrNull(m.fechaFin)) {
+                    let finishDate = m.fechaFin.split('-').reverse().join('/');
+                    dateLabel.innerHTML = `${innitDate} - ${finishDate}`;
+                } else {
+                    dateLabel.innerHTML = `${innitDate} - vigente`;
+                }
+                clientNameLabel.innerHTML = m.nombreCliente || "Cliente";
+                priceLabel.innerHTML = `$${m.precio}`;
+
+                movementType.classList.add("movement-type", "movement-rent");
+                movementText.appendChild(titleLabel);
+                movementText.appendChild(clientNameLabel);
+                movementText.appendChild(priceLabel);
+                movementText.appendChild(dateLabel);
+                movement.appendChild(movementText);
+                movement.appendChild(movementType);
+                break;
+            case 'VENTA':
+                clientNameLabel.innerHTML = m.nombreCliente || "Cliente";
+                priceLabel.innerHTML = `$${m.precio}`;
+
+                movementType.classList.add("movement-type", "movement-sold");
+                movementText.appendChild(titleLabel);
+                movementText.appendChild(clientNameLabel);
+                movementText.appendChild(priceLabel);
+                movementText.appendChild(dateLabel);
+                movement.appendChild(movementText);
+                movement.appendChild(movementType);
+                break;
+        }
+
+        movementList.push(movement);
+    })
+
+    for (var i = movementList.length - 1; i >= 0; i--) {
+        movementsContainerDiv.appendChild(movementList[i]);
+    }
+
+    console.log("Historial desplegado");
+}
