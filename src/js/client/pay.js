f62e139e4d4ebbd2deabb1c81be5ed7cbce3da46
SaulBT <barragansaul26n@gmail.com>
2025-12-30 22:05:15 -0600

    feat: elementos de tarjeta inicializados

--- src/js/client/pay.js
+++ src/js/client/pay.js
@@ -26,7 +26,7 @@ const payMethodSelect = $('payMethodSelect');
 const elementsDiv = $('elementsDiv');
 
 /* Credit Card */
-const carNumberLabel = $('carNumberLabel');
+const cardNumberLabel = $('carNumberLabel');
 const cardNumberInput = document.createElement("input");
 const expirationDateLabel = document.createElement("label");
 const expirationMonthSelect = document.createElement("select");
@@ -58,6 +58,8 @@ async function innit() {
     state.id = params.get("id");
 
     await loadPayMethods();
+    innitCardMethod();
+    loadEvents();
 
     console.log("===PÁGINA INICIALIZADA EXITOSAMENTE===")
 }
@@ -78,10 +80,105 @@ async function loadPayMethods() {
 }
 
 function displayPayMethods() {
+    console.log("Desplegando métodos de pago");
+
     state.payMethods.forEach((m) => {
         const payMethodOption = document.createElement("option");
         payMethodOption.value = m.id;
         payMethodOption.innerHTML = m.nombre;
         payMethodSelect.appendChild(payMethodOption);
     })
+
+    console.log("Métodos de pago desplegados");
+}
+
+function loadEvents() {
+    console.log("Cargando eventos");
+
+    payMethodSelect.addEventListener("change", (e) => {
+        switch((intOrNullpayMethodSelect.value)) {
+            case 1:
+                displayCardMethod();
+                break;
+        }
+    })
+
+    console.log("Eventos cargados");
+}
+
+function innitCardMethod() {
+    console.log("Inicializando el método de pago por tarjeta")
+
+    cardNumberLabel.classList.add("pay-input-label");
+    cardNumberLabel.innerHTML = "Número de tarjeta:";
+    cardNumberInput.classList.add("pay-input");
+    cardNumberInput.type = "number";
+
+    expirationDateLabel.classList.add("pay-input-label");
+    expirationDateLabel.innerHTML = "Fecha de caducidad:";
+    expirationMonthSelect.classList.add("checkbox");
+    const expirationMonthOption = document.createElement("option");
+    expirationMonthOption.value = "NONE"
+    expirationMonthOption.innerHTML = "--";
+    expirationMonthSelect.appendChild(expirationMonthOption);
+    for (var i = 1; i <= 12; i++) {
+        expirationMonthOption.value = i;
+        expirationMonthOption.innerHTML = i;
+        expirationMonthSelect.appendChild(expirationMonthOption);
+    }
+    expirationYearSelect.classList.add("checkbox");
+    const expirationYearOption = document.createElement("option");
+    expirationYearOption.value = "NONE"
+    expirationYearOption.innerHTML = "---";
+    expirationMonthSelect.appendChild(expirationYearOption);
+    for (var i = 2026; i <= 2040; i++) {
+        expirationYearOption.value = i;
+        expirationYearOption.innerHTML = i;
+        expirationMonthSelect.appendChild(expirationYearOption);
+    }
+    securityCodeLabel.classList.add("pay-input-label");
+    securityCodeLabel.innerHTML = "Código de seguridad:";
+    securityCodeInput.classList.add("pay-input");
+    securityCodeInput.type = "number";
+    securityCodeInput.maxLength = "3";
+
+    nameLabel.classList.add("pay-input-label");
+    nameLabel.innerHTML = "Nombre(s):";
+    nameInput.classList.add("pay-input");
+    nameInput.type = "text";
+    lastnameLabel.classList.add("pay-input-label");
+    lastnameLabel.innerHTML = "Apellidos:";
+    nameInput.classList.add("pay-input");
+    nameInput.type = "text";
+
+    addressLabel1.classList.add("pay-input-label");
+    addressLabel1.innerHTML = "Dirección de facturación (Línea 1):";
+    addressInput1.classList.add("pay-input");
+    addressInput1.type = "text";
+    addressLabel2.classList.add("pay-input-label");
+    addressLabel2.innerHTML = "Dirección de facturación (Línea 1):";
+    addressInput2.classList.add("pay-input");
+    addressInput2.type = "text";
+
+    countryLabel.classList.add("pay-input-label");
+    countryLabel.innerHTML = "País:";
+    countrySelect.classList.add("checkbox");
+    countrySelect.innerHTML = `
+        <option value="NONE">Selecciona un país</option>
+        <option value="Mexico">México</option>
+    `;
+    localityLabel.classList.add("pay-input-label");
+    localityLabel.innerHTML = "Localidad:";
+    localityInput.classList.add("pay-input");
+    localityInput.type = "text";
+    zipcodeLabel.classList.add("pay-input-label");
+    zipcodeLabel.innerHTML = "Código postal:";
+    zipcodeInput.classList.add("pay-input");
+    zipcodeInput.type = "number";
+
+    console.log("Método de tarjeta inicializado")
+}
+
+function displayCardMethod() {
+
 }
